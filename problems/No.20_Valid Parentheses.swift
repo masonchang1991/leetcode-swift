4996b5ce42214df690f5604fdce7cbf323653ac1
張凱竣 <mason001@hancock.pro>
2022-02-20 00:13:47 +0800

    [Update] No.20 code

--- problems/No.20_Valid Parentheses.swift
+++ problems/No.20_Valid Parentheses.swift
@@ -10,35 +10,28 @@ import Foundation
 class Solution {
     func isValid(_ s: String) -> Bool {
         
-        var charArray = Array(s)
-        
         // Put close brackets which haven't combine with open brackets
         // first element is the first have to combine with open bracket
         var waitingList: [String.Element] = []
         
-        for char in charArray {
-            // if not, check the first element in waiting list
-            if checkIsTheOpenBracket(by: char) {
+        for char in Array(s) {
+            switch char {
+            case "(", "{", "[":
                 waitingList.insert(getCorrespondingChar(by: char), at: 0)
-            } else {
-                if waitingList.count == 0 {
-                    // that means there is no open bracket in front of this close bracket
+            case ")", "}", "]":
+                // that means there is no open bracket in front of this close bracket
+                if waitingList.count == 0 || char != waitingList.removeFirst() {
                     return false
                 } else {
-                    let firstElemet = waitingList.removeFirst()
-                    if char == firstElemet {
-                        // correct! continueß
-                    } else {
-                        return false
-                    }
+                    // correct! continueßß
                 }
+            default: return false
             }
         }
-        
-        if waitingList.count == 0 {
-            return true
-        } else {
+        if waitingList.count != 0 {
             return false
+        } else {
+            return true
         }
     }
     
@@ -47,16 +40,7 @@ class Solution {
             case "(": return ")"
             case "{": return "}"
             case "[": return "]"
-            default: fatalError() // not gona happen
-        }
-    }
-    
-    func checkIsTheOpenBracket(by char: String.Element) -> Bool {
-        switch char {
-            case "(": return true
-            case "{": return true
-            case "[": return true
-            default: return false
+            default: fatalError()
         }
     }
 }
